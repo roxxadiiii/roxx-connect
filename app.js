1e6759c1c83ea88c9ef02bb3a73cea282597f52d
roxxamay <itsmeadityakumararya@gmail.com>
2024-07-17 17:56:42 +0530

    added bg gradient

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-// const shareButtons = document.querySelectorAll('.tile-share-button');
-// console.log(shareButtons)
-
-// async function copyText(e) {
-//     // Prevent default action
-//     e.preventDefault();
-    
-//     // Find the closest parent <a> tag
-//     const linkElement = e.target.closest('a');
-    
-//     // Get the href attribute from the parent <a> tag
-//     const link = linkElement ? linkElement.getAttribute('href') : '';
-//     try {
-//         await navigator.clipboard.writeText(link);
-//         alert("Copied the text: " + link);
-//     } catch (err) {
-//         console.error(err);
-//     }
-// }
 
 document.addEventListener('DOMContentLoaded', function() {
     // Find all elements with the class 'tile-share-button'
